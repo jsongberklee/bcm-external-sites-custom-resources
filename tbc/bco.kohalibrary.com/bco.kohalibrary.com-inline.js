47239f070e61413af709304e62f6c11a1a1abb9b
Jaesung Song <jsong@berklee.edu>
2016-07-29 16:02:24 -0400

    TBC resources added

---
+++ tbc/bco.kohalibrary.com/bco.kohalibrary.com-inline.js
@@ -0,0 +1,3 @@
+/* JAVASCRIPT: On Application Load */
+jQuery('a.course-reserves-link').attr('href', '//bostonconservatory.edu/library/reserves');jQuery('ul.nav-pills').append('<li><a class="jsong-reference" href="https://docs.google.com/leaf?id=0BxGv2MwTy4BpMTZlNWVmMTktMTZmMy00N2ZlLTgxYjYtYTg3YzhjZWM4OGZj&hl=en_US">Reference</a></li>','<li><a class="jsong-periodicals" href="//bostonconservatory.edu/library/periodicals">Periodicals</a></li>','<li><a class="jsong-search-tips" href="//bostonconservatory.edu/sites/all/files/Koha%20Search%20Tips_1.pdf">Search Tips</a></li>');
+/* / JAVASCRIPT: On Application Load */
