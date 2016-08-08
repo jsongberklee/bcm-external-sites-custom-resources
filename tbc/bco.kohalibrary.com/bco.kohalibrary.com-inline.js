56e33df2f76b734a07291e91974fdf068b8c30af
Jaesung Song <jsong@berklee.edu>
2016-08-08 15:59:30 -0400

    Course reserves link update

--- tbc/bco.kohalibrary.com/bco.kohalibrary.com-inline.js
+++ tbc/bco.kohalibrary.com/bco.kohalibrary.com-inline.js
@@ -1,2 +1,7 @@
 /* JAVASCRIPT: On Application Load */
-jQuery('a.homeLink').text('Library Home');jQuery('a.course-reserves-link').attr('href', '//sites.google.com/a/bostonconservatory.edu/portal/academic-resources/library-resources/course-reserves');jQuery('ul.nav-pills').append('<li><a class="jsong-reference" href="https://docs.google.com/leaf?id=0BxGv2MwTy4BpMTZlNWVmMTktMTZmMy00N2ZlLTgxYjYtYTg3YzhjZWM4OGZj&hl=en_US">Reference</a></li>','<li><a class="jsong-periodicals" href="//sites.google.com/a/bostonconservatory.edu/portal/academic-resources/library-resources/periodicals">Periodicals</a></li>','<li><a class="jsong-search-tips" href="//sites.google.com/a/bostonconservatory.edu/portal/academic-resources/library-resources/library-tips-for-research">Research Tips</a></li>');/* / JAVASCRIPT: On Application Load */
+
+
+jQuery('a.homeLink').text('Library Home');jQuery('a.course-reserves-link').attr('href', '//drive.google.com/drive/u/0/folders/0BxGv2MwTy4BpMzk4YzE4NGEtMzViZC00NjdmLTliN2QtNzM1YzI0YzE1ODcz');jQuery('ul.nav-pills').append('<li><a class="jsong-reference" href="https://docs.google.com/leaf?id=0BxGv2MwTy4BpMTZlNWVmMTktMTZmMy00N2ZlLTgxYjYtYTg3YzhjZWM4OGZj&hl=en_US">Reference</a></li>','<li><a class="jsong-periodicals" href="//sites.google.com/a/bostonconservatory.edu/portal/academic-resources/library-resources/periodicals">Periodicals</a></li>','<li><a class="jsong-search-tips" href="//sites.google.com/a/bostonconservatory.edu/portal/academic-resources/library-resources/library-tips-for-research">Research Tips</a></li>');
+
+
+/* / JAVASCRIPT: On Application Load */
