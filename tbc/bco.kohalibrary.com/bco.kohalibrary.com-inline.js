2280bef2998d70073684af2cb72c76d3cf7f0a73
Jaesung Song <jsong@berklee.edu>
2016-08-17 11:18:37 -0400

    inline search form

--- tbc/bco.kohalibrary.com/bco.kohalibrary.com-inline.js
+++ tbc/bco.kohalibrary.com/bco.kohalibrary.com-inline.js
@@ -7,6 +7,17 @@ jQuery('a.homeLink').text('Library Home');jQuery('a.course-reserves-link').attr(
 /* / JAVASCRIPT: On Application Load */
 
 
+/* JAVASCRIPT: SANDBOX: On Application Load */
+
+jQuery('a.homeLink').text('Library Home');jQuery('a.course-reserves-link').attr('href', '//drive.google.com/drive/u/0/folders/0BxGv2MwTy4BpMzk4YzE4NGEtMzViZC00NjdmLTliN2QtNzM1YzI0YzE1ODcz');jQuery('ul.nav-pills').append('<li><a class="jsong-reference" href="https://docs.google.com/leaf?id=0BxGv2MwTy4BpMTZlNWVmMTktMTZmMy00N2ZlLTgxYjYtYTg3YzhjZWM4OGZj&hl=en_US">Reference</a></li>','<li><a class="jsong-periodicals" href="//sites.google.com/a/bostonconservatory.edu/portal/academic-resources/library-resources/periodicals">Periodicals</a></li>','<li><a class="jsong-search-tips" href="//sites.google.com/a/bostonconservatory.edu/portal/academic-resources/library-resources/library-tips-for-research">Research Tips</a></li>');
+var params={};
+window.location.search.replace(/[?&]+([^=&]+)=([^&]*)/gi,function(str,key,value){params[key] = value;});
+if(params['bcmlib']){params['bcmlib']=decodeURIComponent(params['bcmlib']);setTimeout(function(){angular.element('#searchform input').scope().mast_search.query=params['bcmlib'];angular.element('#searchform input').scope().mastheadSearch();},100);}
+
+/* / JAVASCRIPT: SANDBOX: On Application Load */
+
+
+
 
 /* JAVASCRIPT: opacuserjs */
 $(document).ready(function(){
